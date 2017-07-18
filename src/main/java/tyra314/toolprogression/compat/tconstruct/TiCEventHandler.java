4bf70255f25a22cf17e82a0bae24af4895fb45e6
tyra314 <tyra_oa@icloud.com>
2017-07-18 15:06:25 +0200

    Add back TiC support for 1.12

--- src/main/java/tyra314/toolprogression/compat/tconstruct/TiCEventHandler.java
+++ src/main/java/tyra314/toolprogression/compat/tconstruct/TiCEventHandler.java
@@ -1,8 +1,15 @@
 package tyra314.toolprogression.compat.tconstruct;
 
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import slimeknights.tconstruct.library.events.MaterialEvent;
+import slimeknights.tconstruct.library.materials.HeadMaterialStats;
+import slimeknights.tconstruct.library.materials.IMaterialStats;
+import tyra314.toolprogression.ToolProgressionMod;
+import tyra314.toolprogression.proxy.CommonProxy;
+
 public class TiCEventHandler
 {
-/*    @SubscribeEvent
+    @SubscribeEvent
     public void handleMaterial(MaterialEvent.StatRegisterEvent<IMaterialStats> event)
     {
         String name = event.material.identifier;
@@ -29,5 +36,5 @@ public class TiCEventHandler
                                                String.valueOf(newLevel));
             }
         }
-    }*/
+    }
 }
