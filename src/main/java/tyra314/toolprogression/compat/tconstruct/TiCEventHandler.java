0dcc0bc4d65687b8435c2cdb91abab918f318b51
tyra314 <tyra_oa@icloud.com>
2017-10-23 20:46:48 +0200

    Disables increased harvestlevels of TiC modifiers: diamond and emerald

--- src/main/java/tyra314/toolprogression/compat/tconstruct/TiCEventHandler.java
+++ src/main/java/tyra314/toolprogression/compat/tconstruct/TiCEventHandler.java
@@ -1,8 +1,16 @@
 package tyra314.toolprogression.compat.tconstruct;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import slimeknights.tconstruct.library.events.MaterialEvent;
+import slimeknights.tconstruct.library.events.TinkerCraftingEvent;
 import slimeknights.tconstruct.library.materials.HeadMaterialStats;
+import slimeknights.tconstruct.library.modifiers.IModifier;
+import slimeknights.tconstruct.library.tools.ToolNBT;
+import slimeknights.tconstruct.library.utils.TagUtil;
+import slimeknights.tconstruct.tools.modifiers.ModDiamond;
+import slimeknights.tconstruct.tools.modifiers.ModEmerald;
 import tyra314.toolprogression.ToolProgressionMod;
 import tyra314.toolprogression.proxy.CommonProxy;
 
@@ -35,5 +43,36 @@ public class TiCEventHandler
                                                String.valueOf(newLevel));
             }
         }
+    }
+
+    private int getToolHarvestLevel(ItemStack tinker_tool)
+    {
+        return TagUtil.getToolStats(tinker_tool.getTagCompound()).harvestLevel;
+    }
+
+    private void setToolHarvestLevel(ItemStack tinker_tool, int harvestLevel)
+    {
+        NBTTagCompound tag = tinker_tool.getTagCompound();
+        ToolNBT toolData = TagUtil.getToolStats(tag);
+        toolData.harvestLevel = harvestLevel;
+
+        TagUtil.setToolTag(tag, toolData.get());
+        tinker_tool.setTagCompound(tag);
+    }
+
+    @SubscribeEvent
+    public void handleToolModifier(TinkerCraftingEvent.ToolModifyEvent event)
+    {
+        int harvestLevel = getToolHarvestLevel(event.getItemStack());
+
+        for(IModifier mod : event.getModifiers())
+        {
+            if (mod instanceof ModDiamond || mod instanceof ModEmerald)
+            {
+                harvestLevel--;
+            }
+        }
+
+        setToolHarvestLevel(event.getItemStack(), harvestLevel);
     }
 }
