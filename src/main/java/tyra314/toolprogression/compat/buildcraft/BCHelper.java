tyra314/ToolProgression

commit f4de7b83f66207637945663739ec1889cb98a240

Adds the concept of destroyability

src/main/java/tyra314/toolprogression/handlers/HarvestEventHandler.java
@@ -20,6 +20,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import tyra314.toolprogression.compat.gamestages.GSEventHandler;
 import tyra314.toolprogression.compat.gamestages.GSHelper;
 import tyra314.toolprogression.config.ConfigHandler;
+import tyra314.toolprogression.harvest.BlockOverwrite;
 import tyra314.toolprogression.harvest.HarvestHelper;
 
 import java.util.List;
@@ -134,39 +135,48 @@ public class HarvestEventHandler
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onBreakSpeed(PlayerEvent.BreakSpeed event)
     {
+        // First, get the blockstate, we are actually trying to break
+        IBlockState state = event.getState();
+        if (GSHelper.isLoaded())
+        {
+            state = GSEventHandler.getStagedBlockState(event.getEntityPlayer(), state);
+        }
+
+        // Then, get the break speed we would have, if we would break the actual blockstate
+        // we have to use this value, as OreStages may already have changed the speed
+        float f = getBreakSpeedDefault(event.getEntityPlayer(), state);
+
+        // We now normalize the breakspeed to the one, if the could harvest the block.
+        // Now it get's ugly.
+        // In ForgeHooks::blockStrenght(), from which this event will be invoked indirectly,
+        // there is an if-statement, which distinguishes whether or not, the player can break
+        // the original block. Depending on this, it will slow down break speed to 30 percent.
+        // If we overrule the harvestability, we have to counter this penalty.
+        if (!canHarvestBlockDefault(event.getState(), event.getEntityPlayer()))
+        {
+            f *= 100F / 30F;
+        }
+
         if (!HarvestHelper.canPlayerHarvestBlock(event.getEntityPlayer(),
                 event.getState()))
         {
-            // If we decide that it is not allowed to break the block, just cancel the event.
-            event.setCanceled(true);
-        }
-        else
-        {
-            // If we decide that it is allowed to break the block, we have to fix the break speed
+            // If we decide that the block is not harvestable ...
 
-            // First, get the blockstate, we are actually trying to break
-            IBlockState state = event.getState();
-            if (GSHelper.isLoaded())
+            BlockOverwrite overwrite = ConfigHandler.blockOverwrites.get(state);
+            if (ConfigHandler.all_blocks_destroyable || (overwrite != null && overwrite.destroyable))
             {
-                state = GSEventHandler.getStagedBlockState(event.getEntityPlayer(), state);
+                // ...and the block is destroyable, then we apply the break speed penalty once again.
+                f *= 30F / 100F;
+
             }
-
-            // Then, get the break speed we would have, if we would break the actual blockstate
-            // we have to use this value, as OreStages may already have changed the speed
-            float f = getBreakSpeedDefault(event.getEntityPlayer(), state);
-
-            // Now it get's ugly.
-            // In ForgeHooks::blockStrenght(), from which this event will be invoked indirectly,
-            // there is an if-statement, which distinguishes whether or not, the player can break
-            // the original block. Depending on this, it will slow down break speed to 30 percent.
-            // If we overrule the harvestability, we have to counter this penalty.
-            if (!canHarvestBlockDefault(event.getState(), event.getEntityPlayer()))
+            else
             {
-                f *= 100F / 30F;
+                // ...or else we just cancel the event and thus prevent the block from being broken
+                event.setCanceled(true);
             }
-
-            event.setNewSpeed(f);
         }
+
+        event.setNewSpeed(f);
     }
 
     @SubscribeEvent(priority = EventPriority.LOW)
@@ -235,6 +245,23 @@ public class HarvestEventHandler
 
                 event.getPlayer().addStat(StatList.getBlockStats(event.getState().getBlock()));
             }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onDropEvent(BlockEvent.HarvestDropsEvent event)
+    {
+        if (event.getHarvester() == null)
+        {
+            return;
+        }
+
+        // This disables the drops of harvested blocks, which aren't harvestable, but destroyable
+        if (!HarvestHelper.canPlayerHarvestBlock(event.getHarvester(), event.getState()))
+        {
+            // Well... I can't cancel the event, so lets hope, just setting it to zero will take
+            // take care of that. Expect incoming bugs because of shitz and gigglez. FeelsBadMan
+            event.setDropChance(0);
         }
     }
 }

src/main/java/tyra314/toolprogression/config/ConfigHandler.java
@@ -14,6 +14,8 @@ public class ConfigHandler
     private static final String CATEGORY_MINING_LEVEL = "mining_levels";
     private static final String CATEGORY_TOOLTIP = "tooltip";
 
+    public static boolean all_blocks_destroyable = false;
+
     public static boolean tooltip_enabled = true;
 
     public static boolean waila_enabled = true;
@@ -138,5 +140,9 @@ public class ConfigHandler
 
         dupe_fix_hacky_macky = cfg.getBoolean("dupe_fix_hacky_macky_voodoo_magic_wizardry", CATEGORY_GENERAL,
                 dupe_fix_hacky_macky, "You have a dupe bug? You came in my issue tracker yelling at me? I cried and told you to active this? - If you can answer all these questions with yes, go ahead and set it to true. Otherwise, this setting is better off be left alone. Just like me. Forever alone. T__T");
+
+        all_blocks_destroyable = cfg.getBoolean("all_blocks_destroyable", CATEGORY_GENERAL,
+                all_blocks_destroyable, "Set this to true, if you want to be able to just destroy not harvestable blocks.");
+
     }
 }

src/main/java/tyra314/toolprogression/harvest/BlockOverwrite.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class BlockOverwrite
 {
     private static final String REGEX =
-            "^(?<effective>\\?)?(?<toolclass>[^\\?=]+)=(?<level>-?\\d+)(@(?<hardness>.+))?$";
+            "^(?<force>\\!)?(?<effective>\\?)?(?<toolclass>[^\\?=]+)=(?<level>-?\\d+)(@(?<hardness>.+))?$";
 
     private static final Pattern pattern;
 
@@ -26,6 +26,7 @@ public class BlockOverwrite
     public int level;
     public boolean toolRequired;
     public float hardness;
+    public boolean destroyable;
 
     public BlockOverwrite(String toolclass, int level, boolean toolRequired)
     {
@@ -33,14 +34,9 @@ public class BlockOverwrite
         this.level = level;
         this.toolRequired = toolRequired;
         this.hardness = -1F;
+        this.destroyable = false;
     }
 
-    public BlockOverwrite(String toolclass, int level, boolean toolRequired, float hardness)
-    {
-        this(toolclass, level, toolRequired);
-
-        this.hardness = hardness;
-    }
 
     public static void applyToAllStates(Block block)
     {
@@ -71,26 +67,52 @@ public class BlockOverwrite
             return null;
         }
 
-        boolean toolRequired = matcher.group("effective") == null;
-
         String toolClass = matcher.group("toolclass");
         int level = Integer.parseInt(matcher.group("level"));
+        boolean toolRequired = matcher.group("effective") == null;
+
+        BlockOverwrite b = new BlockOverwrite(toolClass, level, toolRequired);
+
+        if(matcher.group("force") != null)
+        {
+            b.destroyable = true;
+            if(!b.toolRequired)
+            {
+                return null;
+            }
+        }
 
         String hardness = matcher.group("hardness");
+        if (hardness != null)
+        {
+            b.hardness =  Float.parseFloat(hardness);
+        }
 
-        if (hardness == null)
-        {
-            return new BlockOverwrite(toolClass, level, toolRequired);
-        }
-        else
-        {
-            return new BlockOverwrite(toolClass, level, toolRequired, Float.parseFloat(hardness));
-        }
+        return b;
     }
 
     public String getConfig()
     {
-        return (toolRequired ? "" : "?") + toolclass + "=" + String.valueOf(level);
+        StringBuilder str = new StringBuilder();
+
+        if(destroyable)
+        {
+            str.append("!");
+        }
+
+        if(!toolRequired)
+        {
+            str.append("?");
+        }
+
+        str.append(toolclass).append("=").append(level);
+
+        if(hardness >= 0F)
+        {
+            str.append("@").append(hardness);
+        }
+
+        return str.toString();
     }
 
     public void addOverwrite(String toolClass, int level, boolean toolRequired)

commit 0a5f7002400fab8592e93eae5595214ee7ebc040

Fixes several issues introduced with f4de7b8

src/main/java/tyra314/toolprogression/compat/buildcraft/BCHelper.java
@@ -1,46 +1,0 @@
-package tyra314.toolprogression.compat.buildcraft;
-
-import net.minecraft.item.Item;
-import tyra314.toolprogression.ToolProgressionMod;
-
-public class BCHelper
-{
-    public static final String HAMMER_NAME = "buildcraft.api.tools.IToolWrench";
-
-    private static Class<?> buildcraft = null;
-
-
-    static
-    {
-        try
-        {
-            buildcraft = Class.forName(HAMMER_NAME);
-        }
-        catch (ClassNotFoundException ignored)
-        {
-            ToolProgressionMod.logger.info("Buildcraft isn't loaded.");
-        }
-    }
-
-    public static boolean isLoaded()
-    {
-        return buildcraft != null;
-    }
-
-
-    public static boolean isWrench(Item item)
-    {
-        try
-        {
-            Class<?> ToolCore = Class.forName(HAMMER_NAME);
-
-            return ToolCore.isInstance(item);
-
-        }
-        catch (ClassNotFoundException ignored)
-        {
-        }
-
-        return false;
-    }
-}

src/main/java/tyra314/toolprogression/compat/cofh/CoFHHelper.java
@@ -10,7 +10,6 @@ import java.lang.reflect.Method;
 public class CoFHHelper
 {
     public static final String CLASS_NAME = "cofh.core.item.tool.ItemToolCore";
-    public static final String HAMMER_NAME = "cofh.api.item.IToolHammer";
 
     private static Class<?> cofh_core = null;
 
@@ -62,22 +61,6 @@ public class CoFHHelper
     }
 
     public static boolean isInstance(Item item)
-    {
-        try
-        {
-            Class<?> ToolCore = Class.forName(CLASS_NAME);
-
-            return ToolCore.isInstance(item);
-
-        }
-        catch (ClassNotFoundException ignored)
-        {
-        }
-
-        return false;
-    }
-
-    public static boolean isHammer(Item item)
     {
         try
         {

src/main/java/tyra314/toolprogression/compat/rftools/RFHelper.java
@@ -1,48 +1,0 @@
-package tyra314.toolprogression.compat.rftools;
-
-import net.minecraft.item.Item;
-import tyra314.toolprogression.ToolProgressionMod;
-
-public class RFHelper
-{
-
-    public static final String WRENCH_NAME = "mcjty.lib.api.smartwrench.SmartWrench";
-
-    private static Class<?> rftools = null;
-
-    static
-    {
-        try
-        {
-            rftools = Class.forName(WRENCH_NAME);
-        }
-        catch (ClassNotFoundException ignored)
-        {
-            ToolProgressionMod.logger.info("RFTools isn't loaded.");
-        }
-    }
-
-    public static boolean isLoaded()
-    {
-        return rftools != null;
-    }
-
-
-    public static boolean isWrench(Item item)
-    {
-        try
-        {
-            Class<?> ToolCore = Class.forName(WRENCH_NAME);
-
-            return ToolCore.isInstance(item);
-
-        }
-        catch (ClassNotFoundException ignored)
-        {
-        }
-
-        return false;
-    }
-}
-
-

src/main/java/tyra314/toolprogression/handlers/HarvestEventHandler.java
@@ -7,7 +7,6 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Enchantments;
 import net.minecraft.init.MobEffects;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.NonNullList;
@@ -19,11 +18,8 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import tyra314.toolprogression.compat.buildcraft.BCHelper;
-import tyra314.toolprogression.compat.cofh.CoFHHelper;
 import tyra314.toolprogression.compat.gamestages.GSEventHandler;
 import tyra314.toolprogression.compat.gamestages.GSHelper;
-import tyra314.toolprogression.compat.rftools.RFHelper;
 import tyra314.toolprogression.config.ConfigHandler;
 import tyra314.toolprogression.harvest.BlockOverwrite;
 import tyra314.toolprogression.harvest.HarvestHelper;
@@ -127,7 +123,7 @@ public class HarvestEventHandler
         {
             for (ItemStack drop : drops)
             {
-                state.getBlock().spawnAsEntity(world, pos, drop);
+                Block.spawnAsEntity(world, pos, drop);
             }
         }
     }
@@ -185,20 +181,6 @@ public class HarvestEventHandler
         event.setNewSpeed(f);
     }
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    void onHarvestCheck(PlayerEvent.HarvestCheck event)
-    {
-        // actually I'd say, this event is meaningless, however I fix FakePlayers with that :C
-
-        if (event.getTargetBlock() == null || event.getEntityPlayer() == null)
-        {
-            return;
-        }
-
-        event.setCanHarvest(HarvestHelper.canPlayerHarvestBlock(event.getEntityPlayer(),
-                event.getTargetBlock()));
-    }
-
     @SubscribeEvent(priority = EventPriority.LOW)
     public void onBreakEvent(BlockEvent.BreakEvent event)
     {
@@ -253,7 +235,7 @@ public class HarvestEventHandler
 
                         for (ItemStack drop : drops)
                         {
-                            block.spawnAsEntity(event.getWorld(), event.getPos(), drop);
+                            Block.spawnAsEntity(event.getWorld(), event.getPos(), drop);
                         }
                     }
                 }
@@ -266,65 +248,26 @@ public class HarvestEventHandler
                 event.getPlayer().addStat(StatList.getBlockStats(event.getState().getBlock()));
             }
         }
-    }
-
-    private boolean isWrench(ItemStack stack)
-    {
-        Item item = stack.getItem();
-
-        if (BCHelper.isLoaded() && BCHelper.isWrench(item))
+        else if (!HarvestHelper.canPlayerHarvestBlock(event.getPlayer(), event.getState()))
         {
-            return true;
-        }
-
-        if (CoFHHelper.isLoaded() && CoFHHelper.isHammer(item))
-        {
-            return true;
-        }
-
-        if (RFHelper.isLoaded() && RFHelper.isWrench(item))
-        {
-            return true;
-        }
-
-        // It seems like I don't need to check for IC2 wrenches. God bless.
-
-        return false;
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void onDropEvent(BlockEvent.HarvestDropsEvent event)
-    {
-        // Okay, I use this event to accomplish one specific behavior. I want to be able to let
-        // players break a block, but it shall not drop anything. Therefore, I neither can't
-        // cancel the BreakSpeed nor the BlockBreak events. However, in this event, I can't know
-        // which and how the block was broken. So I must use some guessing here.
-
-
-        // if there is no harvester, I can't decide anything. So skip it.
-        // also, if we have a FakePlayer in front of our face, we give up here
-        if (event.getHarvester() == null || event.getHarvester() instanceof FakePlayer)
-        {
-            return;
-        }
-
-        if (!HarvestHelper.canPlayerHarvestBlock(event.getHarvester(), event.getState()))
-        {
-            if (isWrench(event.getHarvester().getHeldItemMainhand()))
+            // if there is no harvester, I can't decide anything. So skip it.
+            // also, if we have a FakePlayer in front of our face, we give up here
+            if (event.getPlayer() == null || event.getPlayer() instanceof FakePlayer)
             {
-                // if the harvester has a wrench in his hand, I assume that this wrench was used
-                // and therefore it can circumvent my checks.
-
-                // Fortunately, a wrench isn't a good pickaxe, so it should be fine harvesting stuff
-
                 return;
             }
 
-            // This disables the drops of harvested blocks, which aren't harvestable, but destroyable
+            // If we decide that the block is not harvestable ...
+            IBlockState state = event.getWorld().getBlockState(event.getPos());
+            BlockOverwrite overwrite = ConfigHandler.blockOverwrites.get(state);
+            if (ConfigHandler.all_blocks_destroyable ||
+                (overwrite != null && overwrite.destroyable))
 
-            // Well... I can't cancel the event, so lets hope, just setting it to zero will take
-            // take care of that. Expect incoming bugs because of shitz and gigglez. FeelsBadMan
-            event.setDropChance(0);
+            {
+                // This should not drop anything
+                event.setCanceled(true);
+                event.getWorld().setBlockToAir(event.getPos());
+            }
         }
     }
 }