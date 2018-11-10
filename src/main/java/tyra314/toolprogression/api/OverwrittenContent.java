5ec41b46a9043dc7e91990a3d06ac7d10a3e51b3
tyra314 <tyra_oa@icloud.com>
2018-11-10 18:09:39 +0100

    Clarifications on API intent

--- src/main/java/tyra314/toolprogression/api/OverwrittenContent.java
+++ src/main/java/tyra314/toolprogression/api/OverwrittenContent.java
@@ -4,6 +4,16 @@ import tyra314.toolprogression.harvest.*;
 import java.util.*;
 import net.minecraft.item.Item.ToolMaterial;
 
+
+/**
+ * Public API of ToolProgression
+ *
+ * The intent of this class is to provide an interface to other mods,
+ * such that they can **READ** all overwrites and the mining levels used.
+ *
+ * While they may allow to change ToolProgression internals right now,
+ * you shouldn't depend on that behavior!
+ */
 public class OverwrittenContent
 {
     public static final Map<String,BlockOverwrite> blocks = new HashMap<>();
