ccb31f0a5d535e3c70550901bb963e58ead6b14a
tyra314 <tyra_oa@icloud.com>
2020-09-13 18:38:44 +0200

    Fix Metallurgy materials cannot be overwritten

--- src/main/java/tyra314/toolprogression/ToolProgressionMod.java
+++ src/main/java/tyra314/toolprogression/ToolProgressionMod.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 import tyra314.toolprogression.command.ToolProgressionCommand;
 import tyra314.toolprogression.proxy.CommonProxy;
 
-@Mod(modid = ToolProgressionMod.MODID, certificateFingerprint = ToolProgressionMod.SHA_HASH, version = ToolProgressionMod.VERSION, dependencies = "before:tconstruct;after:gamestages;after:orestages", acceptedMinecraftVersions = "[1.12,1.13)")
+@Mod(modid = ToolProgressionMod.MODID, certificateFingerprint = ToolProgressionMod.SHA_HASH, version = ToolProgressionMod.VERSION, dependencies = "before:tconstruct;after:gamestages;after:orestages;before:metallurgy", acceptedMinecraftVersions = "[1.12,1.13)")
 public class ToolProgressionMod
 {
     public static final String MODID = "toolprogression";
